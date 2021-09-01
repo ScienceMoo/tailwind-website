d15340a9530f19da14981c56aa93a9c3ad8601b2
Marie <snailgirlmarie@gmail.com>
2021-08-31 21:17:59 -0300

    update

--- pages/header.js
+++ pages/header.js
@@ -7,17 +7,19 @@ export default function Header() {
             <img
               src="/hugo_nft.svg"
               alt="Age 12 to Married NFT"
-              className="h-16 md:h-24 object-contain"
+              className="h-12 sm:h-16 md:h-24 object-contain"
             />
             <div>
-              <h1 className="text-xl md:text-3xl">Age 12 to Married</h1>
-              <div className="text-xl self-start mb-4">
+              <h1 className="text-md sm:text-xl md:text-3xl">
+                Age 12 to Married
+              </h1>
+              <div className="text-xs sm:text-sm md:text-xl self-start">
                 I Took a Photo Every Day!
               </div>
             </div>
           </div>
         </a>
-        <button className="py-2 px-4 bg-blue-400 rounded-lg font-bold h-12 text-sm md:text-md">
+        <button className="py-2 px-4 bg-blue-400 rounded-lg font-bold h-10 md:h-12 text-sm md:text-md">
           Contact
         </button>
       </div>
